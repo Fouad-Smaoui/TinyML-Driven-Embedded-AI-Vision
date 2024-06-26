4dbe2be6c86bbe8ac8e09ea7972deabb077991e6
Fouad Smaoui <Fouad.Smaoui@ensam.eu>
2024-06-27 01:07:23 +0200

    added kalman filter for optimization

---
+++ facial_landmarks_tracking/kalman_filter.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+class KalmanFilter:
+    def __init__(self):
+        self.kalman = cv2.KalmanFilter(4, 2)
+        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+        self.kalman.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 5, 0], [0, 0, 0, 5]], np.float32) * 0.03
+
+    def predict(self):
+        return self.kalman.predict()
+
+    def correct(self, x, y):
+        return self.kalman.correct(np.array([[np.float32(x)], [np.float32(y)]]))
