45e78e329b2f7eaa5753b359cdc2e18573b7c8d4
Fouad Smaoui <Fouad.Smaoui@ensam.eu>
2024-07-04 22:08:19 +0200

    Enhanced face tracking with Kalman filter and motion prediction

--- facial_landmarks_tracking/kalman_filter.py
+++ facial_landmarks_tracking/kalman_filter.py
@@ -4,10 +4,30 @@ import cv2
 class KalmanFilter:
     def __init__(self):
         self.kalman = cv2.KalmanFilter(4, 2)
-        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
-        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
-        self.kalman.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 5, 0], [0, 0, 0, 5]], np.float32) * 0.03
-
+        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0], 
+                                                  [0, 1, 0, 0]], np.float32)
+        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0], 
+                                                 [0, 1, 0, 1], 
+                                                 [0, 0, 1, 0], 
+                                                 [0, 0, 0, 1]], np.float32)
+        
+        # Process noise covariance
+        self.kalman.processNoiseCov = np.array([[1, 0, 0, 0], 
+                                                [0, 1, 0, 0], 
+                                                [0, 0, 1, 0], 
+                                                [0, 0, 0, 1]], np.float32) * 0.03
+        # Measurement noise covariance
+        self.kalman.measurementNoiseCov = np.array([[1, 0], 
+                                                    [0, 1]], np.float32) * 1
+        
+        # Error covariance matrix
+        self.kalman.errorCovPost = np.eye(4, dtype=np.float32)
+        
+        # Initial state (location and velocity)
+        self.kalman.statePost = np.array([[0], 
+                                          [0], 
+                                          [0], 
+                                          [0]], np.float32)
     def predict(self):
         return self.kalman.predict()
 
